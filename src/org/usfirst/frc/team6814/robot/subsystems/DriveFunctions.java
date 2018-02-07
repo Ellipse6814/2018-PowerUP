d41751bb75ecec1414dcd7bb363f713ecbfb0bf8
JakeT24 <jacob@envirovue.com>
2018-02-07 11:54:09 -0800

    Added Drive Functions Subsystem, AutoSequence Class
    AutoSequence does nothing yet, Drive Functions is a way to group all the Encoder Command series together.

--- src/org/usfirst/frc/team6814/robot/subsystems/DriveFunctions.java
+++ src/org/usfirst/frc/team6814/robot/subsystems/DriveFunctions.java
@@ -1,18 +1,37 @@
 package org.usfirst.frc.team6814.robot.subsystems;
 
 import org.usfirst.frc.team6814.robot.RobotMap;
+import org.usfirst.frc.team6814.robot.commands.EncoderBackward;
+import org.usfirst.frc.team6814.robot.commands.EncoderForward;
+import org.usfirst.frc.team6814.robot.commands.EncoderLeft;
+import org.usfirst.frc.team6814.robot.commands.EncoderRight;
 
 import edu.wpi.first.wpilibj.Encoder;
 
 public class DriveFunctions {
-	public static void drive(double _distance) {
-		double distance = _distance;
-		while(true) {
-			if(RobotMap.rightEnc.getDistance() > distance) {
-				break;
-			}else {
-				RobotMap.driveBot.tankDrive(.5, .5);
-			}
-		}
+	public static void driveForward(double speed, int feet, int inches) {
+		
+		EncoderForward encForward = new EncoderForward(speed, feet, inches);
+		
+		
 	}
+	
+	public static void driveBackward(double speed, int feet, int inches) {
+		
+		EncoderBackward encBackward = new EncoderBackward(speed, feet, inches);
+		
+	}
+	
+	public static void driveLeft(double speed, int feet, int inches) {
+		
+		EncoderLeft encleft = new EncoderLeft(speed, feet, inches);
+		
+	}
+	
+	public static void driveRight(double speed, int feet, int inches) {
+	
+		EncoderRight encRight = new EncoderRight(speed, feet, inches);
+			
+	}
+	
 }
