71042684bfbda8c7173d85ed0a1a49ccfb145f2a
magilla121 <liev.haroche@sonomaacademy.org>
2018-02-12 11:47:49 -0800

    Added a drive forward function that doesnt need a command class to work

--- src/org/usfirst/frc/team6814/robot/subsystems/DriveFunctions.java
+++ src/org/usfirst/frc/team6814/robot/subsystems/DriveFunctions.java
@@ -1,16 +1,23 @@
 package org.usfirst.frc.team6814.robot.subsystems;
 
 import org.usfirst.frc.team6814.robot.commands.EncoderBackward;
-import org.usfirst.frc.team6814.robot.commands.EncoderForward;
 import org.usfirst.frc.team6814.robot.commands.EncoderLeft;
 import org.usfirst.frc.team6814.robot.commands.EncoderRight;
+import org.usfirst.frc.team6814.robot.RobotMap;
 
 public class DriveFunctions {
-	public static void driveForward(double speed, double feet, double inches) {
-		
-		EncoderForward encForward = new EncoderForward(speed, feet, inches);
-		
-		
+	public static void driveForward(double _speed, double feet, double inches) {
+		double distance = (feet * 12) + inches;
+		double speed = _speed;
+		//We're only going to run one of these functions at a time so using a while loop is not an issue
+		while(RobotMap.rightEnc.getDistance() < distance) {
+			//this function makes sure the robot's speed changes over time as opposed to sudden stops and starts
+			double driveSpeed = smoothSpeed(distance, RobotMap.rightEnc.getDistance());
+			//send em forward!
+			RobotMap.driveBot.tankDrive(driveSpeed, driveSpeed);
+		}
+		//each auton command assumes the encoder starts at zero
+		RobotMap.rightEnc.reset();
 	}
 	
 	public static void driveBackward(double speed, double feet, double inches) {
@@ -30,5 +37,14 @@ public class DriveFunctions {
 		EncoderRight encRight = new EncoderRight(speed, feet, inches);
 			
 	}
-	
+	//uses a function to make the robot accelerate and brake gradually as opposed to just jolting forward and stopping at the last second.
+	public static double smoothSpeed(double totalDistance, double currentDistance) {
+		double smoothSpeed;
+		//the length of the parabola scales based on how far the robot needs to go
+		double stretch = 1/totalDistance;
+		//calculates an appropriate speed based on a reading from the encoder
+		smoothSpeed = Math.pow(stretch * currentDistance - 1, 2) * -1 + 1;
+		return smoothSpeed;
+		//all done!
+	}
 }
