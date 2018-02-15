e1bf318d240d30b29922e83462f8e8acb3cea6a8
magilla121 <liev.haroche@sonomaacademy.org>
2018-02-14 18:08:19 -0800

    Bugs === "Fixed";

--- src/org/usfirst/frc/team6814/robot/commands/RaiseElevator.java
+++ src/org/usfirst/frc/team6814/robot/commands/RaiseElevator.java
@@ -2,7 +2,6 @@ package org.usfirst.frc.team6814.robot.commands;
 
 import org.usfirst.frc.team6814.robot.RobotMap;
 
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class RaiseElevator extends Command {
