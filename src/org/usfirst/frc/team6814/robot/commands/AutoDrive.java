0c91542c373bdbba689e331d6566c7fbe2dcb540
magilla121 <liev.haroche@sonomaacademy.org>
2018-03-16 16:48:59 -0400

    Encoder Update
    The bot now uses the encoder distance as opposed to a set number of seconds to now when to stop.

--- src/org/usfirst/frc/team6814/robot/commands/AutoDrive.java
+++ src/org/usfirst/frc/team6814/robot/commands/AutoDrive.java
@@ -4,17 +4,24 @@ import org.usfirst.frc.team6814.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.Encoder;
 
 public class AutoDrive extends Command{
 	private boolean currentState = false;
 	private DifferentialDrive robot = RobotMap.driveBot;
+	private Encoder enc = RobotMap.rightEnc;
+	//distance in inches = 132; in feet: 11
+	private double distance = 132;
+	private double wheelCircumference = Math.PI * 6;
 	
 	@Override
 	protected void execute() {
-		double currentTime = RobotMap.timer.get();
-		if(currentTime < 3) {
+		double rotations = enc.get()/128;
+		double distTraveled = rotations * wheelCircumference;
+		if(distTraveled < distance) {
 			robot.tankDrive(1, 1);
 		} else {
+			robot.tankDrive(0,0);
 			currentState = true;
 		}
 	}
